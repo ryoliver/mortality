cd5f46821086dc8eff6290e9e286b1288f3de6e8
Ruth Oliver <rutholiver@Ruths-MacBook-Pro.local>
2024-08-27 11:37:38 -0700

    add print statements

--- src/poc/annotate-tracks.R
+++ src/poc/annotate-tracks.R
@@ -8,10 +8,14 @@ rm(list = ls())
 
 #........................source functions........................
 
+print("source functions...")
+
 list.files(here("src", "funs","auto"), full.names = TRUE) %>%
   walk(source)
 
 #..........................read in data..........................
+
+print("read in data...")
 
 # gps data
 gps_file <- select_file(here("analysis", "data_cleaned"), "gps")
@@ -21,6 +25,8 @@ gps <- read_csv(here("analysis", "data_cleaned", gps_file))
 ghm <- terra::rast(here("data", "gHM.tif"))
 
 #..................prep GPS data for annotation..................
+
+print("prep GPS data for annotation...")
 
 # convert to sf object
 gps_sf <- st_as_sf(gps, coords = c("longitude", "latitude"), crs = 4326) 
@@ -33,10 +39,14 @@ gps_spatvect <- vect(gps_sf)
 
 #........................annotate tracks.........................
 
+print("annotate tracks...")
+
 # annotate GPS data with GHM layer
 gps_annotated_ghm <- terra::extract(y = gps_spatvect, x = ghm)
 
 #........................clean up output.........................
+
+print("clean up output...")
 
 # bind annotations to original GPS data frame
 gps_annotated <- cbind(gps, gps_annotated_ghm) %>%
@@ -44,6 +54,8 @@ gps_annotated <- cbind(gps, gps_annotated_ghm) %>%
   select(-ID)
 
 #.........................write out data.........................
+
+print("write out data...")
 
 output_filename <- paste0(str_sub(gps_file, start = 1, end = -5),"_annotated.csv")
 write_csv(x = gps_annotated, file = here("analysis", "data_annotated", output_filename))
